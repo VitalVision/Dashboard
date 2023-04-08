f9d43b8689ef845efa225627fd51a1ba841f4394
Junx <junxiongong2@gmail.com>
2023-04-08 19:05:42 +0800

    remove easy

--- src/components/game/VrGameCard.js
+++ src/components/game/VrGameCard.js
@@ -1,36 +1,35 @@
 // components/VrGameCard.js
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const VrGameCard = ({ game }) => {
-  const { imageUrl, name, description, rehabTarget, difficulty, gameId } = game;
+	const { imageUrl, name, description, rehabTarget, difficulty, gameId } = game;
 
-  return (
-    <div className="w-full p-2 md:w-1/3">
-      <Link href={`/admin/games/${gameId}`}>
-        <a className="block">
-          <div className="overflow-hidden bg-white rounded-lg shadow-md">
-            <div className="relative w-full h-64">
-              <Image
-                src={imageUrl}
-                alt={name}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-              />
-            </div>
-            <div className="p-4">
-              <h2 className="text-xl font-bold">{name}</h2>
-              <p className="font-bold">{description}</p>
-              <p className="text-gray-600">Rehab Target: {rehabTarget}</p>
-              <p className="text-gray-600">Difficulty: {difficulty}</p>
-            </div>
-          </div>
-        </a>
-      </Link>
-    </div>
-  );
+	return (
+		<div className="w-full p-2 md:w-1/3">
+			<Link href={`/admin/games/${gameId}`}>
+				<a className="block">
+					<div className="overflow-hidden bg-white rounded-lg shadow-md">
+						<div className="relative w-full h-64">
+							<Image
+								src={imageUrl}
+								alt={name}
+								layout="fill"
+								objectFit="cover"
+								objectPosition="center"
+							/>
+						</div>
+						<div className="p-4">
+							<h2 className="text-xl font-bold">{name}</h2>
+							<p className="font-bold">{description}</p>
+							<p className="text-gray-600">Rehab Target: {rehabTarget}</p>
+						</div>
+					</div>
+				</a>
+			</Link>
+		</div>
+	);
 };
 
 export default VrGameCard;
