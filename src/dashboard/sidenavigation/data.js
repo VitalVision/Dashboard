6952d89d40d8aa63c47b1be405564644bfd9e111
Junx <junxiongong2@gmail.com>
2023-04-09 14:05:57 +0800

    add redirects

--- src/dashboard/sidenavigation/data.js
+++ src/dashboard/sidenavigation/data.js
@@ -1,44 +1,44 @@
-import HomeIcon from './icons/home';
-import PeopleIcon from './icons/people';
-import AnalyticsIcon from './icons/analytics';
-import MonitoringIcon from './icons/monitoring';
-import DemographicsIcon from './icons/demographics';
-import { getNextPatientId } from '../../utils/utils';
-import DashboardIcon from './icons/dashboard';
+import HomeIcon from "./icons/home";
+import PeopleIcon from "./icons/people";
+import AnalyticsIcon from "./icons/analytics";
+import MonitoringIcon from "./icons/monitoring";
+import DemographicsIcon from "./icons/demographics";
+import { getNextPatientId } from "../../utils/utils";
+import DashboardIcon from "./icons/dashboard";
 
-const patientId = getNextPatientId()
+const patientId = getNextPatientId();
 
 const data = [
-  {
-    title: 'Home',
-    icon: <HomeIcon />,
-    link: '/',
-  },
-  {
-    title: 'Patients Dashboard',
-    icon: <DashboardIcon />,
-    link: `/patient/${patientId}/dashboard`,
-  },
-  {
-    title: 'Patients',
-    icon: <PeopleIcon />,
-    link: '/admin/patients',
-  },
-  {
-    title: 'Analytics',
-    icon: <AnalyticsIcon />,
-    link: '/admin/analytics',
-  },
-  {
-    title: 'Games',
-    icon: <MonitoringIcon />,
-    link: '/admin/games',
-  },
-  {
-    title: 'Profile',
-    icon: <DemographicsIcon />,
-    link: '/admin/profile',
-  },
+	{
+		title: "Home",
+		icon: <HomeIcon />,
+		link: "/",
+	},
+	{
+		title: "Patients Dashboard",
+		icon: <DashboardIcon />,
+		link: `/patient/${patientId}/dashboard`,
+	},
+	{
+		title: "Patients",
+		icon: <PeopleIcon />,
+		link: "/admin/patients",
+	},
+	{
+		title: "Analytics",
+		icon: <AnalyticsIcon />,
+		link: "/admin/analytics",
+	},
+	{
+		title: "Games",
+		icon: <MonitoringIcon />,
+		link: "/admin/games",
+	},
+	// {
+	//   title: 'Profile',
+	//   icon: <DemographicsIcon />,
+	//   link: '/admin/profile',
+	// },
 ];
 
 export default data;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,35 @@
-import Content from '../components/content';
+import Content from "../components/content";
 
 export default function HomePage() {
-  return <Content>
-    <h1 className="mb-6 text-4xl font-bold text-center">Welcom to Vital Vision</h1>
-    <p className="mb-6 text-xl font-bold text-center">We are transforming Rehab with VR</p>
-  </Content>;
+	if (isFirstTimeVisitor) {
+		return <p>Redirecting to landing page...</p>;
+	}
+
+	return (
+		<Content>
+			<h1 className="mb-6 text-4xl font-bold text-center">
+				Welcom to Vital Vision
+			</h1>
+			<p className="mb-6 text-xl font-bold text-center">
+				We are transforming Rehab with VR
+			</p>
+		</Content>
+	);
+}
+
+export async function getServerSideProps(context) {
+	const isFirstTimeVisitor = true;
+
+	if (isFirstTimeVisitor) {
+		return {
+			redirect: {
+				destination: "/landing",
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: { isFirstTimeVisitor: false },
+	};
 }
